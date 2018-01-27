161cea2d7bbd0e0c07bbb1442467f9dd8accb8bd
Clovin <wxxzhuang@gmail.com>
2018-01-27 14:53:49 +0800

    solve 561

---
+++ 476 Number Complement/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Clovin on 27/01/2018.
+ */
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var findComplement = function (num) {
+  // TODO: solve
+  return ~(-num)
+}
+
+console.log(findComplement(5))
