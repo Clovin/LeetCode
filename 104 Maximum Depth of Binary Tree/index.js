22d6a1482637e827f65831fef77efdcfc8e0a48f
Clovin <wxxzhuang@gmail.com>
2018-01-28 02:56:33 +0800

    solve 104

--- 104 Maximum Depth of Binary Tree/index.js
+++ 104 Maximum Depth of Binary Tree/index.js
@@ -13,5 +13,26 @@
  * @return {number}
  */
 var maxDepth = function (root) {
-  //TODO: solve
+  if (root === null) {
+    return 0
+  }
+
+  let depth = 0
+  let queue = []
+  queue.push(root)
+  while (queue.length !== 0) {
+    ++depth
+    for (let i = 0, n = queue.length; i < n; ++i) {
+      let p = queue.shift()
+
+      if (p.left !== null) {
+        queue.push(p.left)
+      }
+      if (p.right !== null) {
+        queue.push(p.right)
+      }
+    }
+  }
+
+  return depth
 }
