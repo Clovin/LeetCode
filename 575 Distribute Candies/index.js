77f27b4ddf2000f867327aa860aed172b1222659
Clovin <wxxzhuang@gmail.com>
2018-02-01 02:10:15 +0800

    solve 575

--- 575 Distribute Candies/index.js
+++ 575 Distribute Candies/index.js
@@ -1,4 +1,16 @@
 /**
  * Created by Clovin on 31/01/2018.
  */
-//TODO: solve
+/**
+ * @param {number[]} candies
+ * @return {number}
+ */
+var distributeCandies = function (candies) {
+  let candiesSet = new Set()
+
+  for (let i = 0; i < candies.length; i++) {
+    candiesSet.add(candies[i])
+  }
+
+  return candies.length >= 2 * candiesSet.size ? candiesSet.size : Math.floor(candiesSet.size / 2 + (candies.length - candiesSet.size) / 2)
+}
