4b99b39ddb0a481e6aa510c9b47f928f90927e52
Clovin <wxxzhuang@gmail.com>
2018-02-07 04:15:48 +0800

    solve 35

---
+++ 35 Search Insert Position/index.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Clovin on 07/02/2018.
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === target) {
+      return i
+    }
+
+    if (nums[i] > target) {
+      return i
+    }
+  }
+
+  return nums.length
+}
