2858e9d855ceb3afca4de6130f6e3c42fa770b3a
Clovin <wxxzhuang@gmail.com>
2018-02-16 15:17:16 +0800

    solve 695

--- 695 Max Area of Island/index.js
+++ 695 Max Area of Island/index.js
@@ -6,5 +6,56 @@
  * @return {number}
  */
 var maxAreaOfIsland = function (grid) {
-  // TODO: solve
+  let queue = []
+  let result = 0
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      let temp = 0
+
+      if (grid[i][j] === 1) {
+        queue.push([i, j])
+        grid[i][j] = 0
+        temp = 1
+      }
+
+      while (queue.length !== 0) {
+        let tempPoint = queue.shift()
+
+        // left
+        if (tempPoint[1] - 1 >= 0 && grid[tempPoint[0]][tempPoint[1] - 1] === 1) {
+          temp++
+          grid[tempPoint[0]][tempPoint[1] - 1] = 0
+          queue.push([tempPoint[0], tempPoint[1] - 1])
+        }
+
+        // bottom
+        if (tempPoint[0] + 1 < grid.length && grid[tempPoint[0] + 1][tempPoint[1]] === 1) {
+          temp++
+          grid[tempPoint[0] + 1][tempPoint[1]] = 0
+          queue.push([tempPoint[0] + 1, tempPoint[1]])
+        }
+
+        // right
+        if (tempPoint[1] + 1 < grid[i].length && grid[tempPoint[0]][tempPoint[1] + 1] === 1) {
+          temp++
+          grid[tempPoint[0]][tempPoint[1] + 1] = 0
+          queue.push([tempPoint[0], tempPoint[1] + 1])
+        }
+
+        // top
+        if (tempPoint[0] - 1 >= 0 && grid[tempPoint[0] - 1][tempPoint[1]] === 1) {
+          temp++
+          grid[tempPoint[0] - 1][tempPoint[1]] = 0
+          queue.push([tempPoint[0] - 1, tempPoint[1]])
+        }
+      }
+
+      if (temp > result) {
+        result = temp
+      }
+    }
+  }
+
+  return result
 }
