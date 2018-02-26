9f58a4d16f1d92dc497f77a070d3bf069108747a
Clovin <wxxzhuang@gmail.com>
2018-02-26 16:21:56 +0800

    solve 141

---
+++ 141 Linked List Cycle/index.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Clovin on 26/02/2018.
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+  let [slow, fast] = [head, head]
+  while (fast !== null && fast.next !== null) {
+    slow = slow.next
+    fast = fast.next.next
+    if (slow === fast) {
+      return true
+    }
+  }
+  return false
+}
