c1892cb49fa1d84c311aa4b0b591884afdeb3b14
Clovin <wxxzhuang@gmail.com>
2018-02-28 03:20:35 +0800

    solve 172

---
+++ 172 Factorial Trailing Zeroes/index.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Clovin on 28/02/2018.
+ */
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var trailingZeroes = function (n) {
+  if (n < 5) {
+    return 0
+  }
+
+  var count = 0
+  while (n >= 5) {
+    count += Math.floor(n / 5)
+    n = parseInt(n / 5)
+  }
+
+  return count
+}
