b85df8d3e8ad2d14ef1c3aaf8d5473ebaef70c3d
Clovin <wxxzhuang@gmail.com>
2018-03-02 20:50:13 +0800

    solve 720

---
+++ 458 Poor Pigs/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Clovin on 02/03/2018.
+ */
+/**
+ * @param {number} buckets
+ * @param {number} minutesToDie
+ * @param {number} minutesToTest
+ * @return {number}
+ */
+var poorPigs = function (buckets, minutesToDie, minutesToTest) {
+  
+}
--- 720 Longest Word in Dictionary/index.js
+++ 720 Longest Word in Dictionary/index.js
@@ -6,5 +6,28 @@
  * @return {string}
  */
 var longestWord = function (words) {
-//TODO: solve
+  let tempMap = new Map()
+  let result = ''
+  let max = 0
+
+  words.sort((a, b) => a.localeCompare(b))
+
+  for (let i = 0; i < words.length; i++) {
+    let temp = words[i].slice(0, words[i].length - 1)
+
+    if (tempMap.has(temp)) {
+      tempMap.set(words[i], tempMap.get(temp) + 1)
+
+      if (tempMap.get(words[i]) > max) {
+        max = tempMap.get(words[i])
+        result = words[i]
+      }
+    }
+
+    if (!tempMap.has(words[i]) && words[i].length === 1) {
+      tempMap.set(words[i], 0)
+    }
+  }
+
+  return result === '' ? words[0] : result
 }
