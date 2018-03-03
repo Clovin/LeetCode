2abb8b4ee99d42ef69264c0ebb17e32079b23b01
Clovin <wxxzhuang@gmail.com>
2018-03-03 20:41:01 +0800

    solve 303

---
+++ 303 Range Sum Query - Immutable/index.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Clovin on 03/03/2018.
+ */
+/**
+ * @param {number[]} nums
+ */
+var NumArray = function (nums) {
+  this.nums = nums
+}
+
+/**
+ * @param {number} i
+ * @param {number} j
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function (i, j) {
+  let sum = 0
+  for (; i <= j; i++) {
+    sum += this.nums[i]
+  }
+  return sum
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = Object.create(NumArray).createNew(nums)
+ * var param_1 = obj.sumRange(i,j)
+ */
