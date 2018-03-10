ebcca93986fcf1904e2c04b63b9f20e91e9abc63
Clovin <wxxzhuang@gmail.com>
2018-03-10 19:11:26 +0800

    solve 479

---
+++ 479 Largest Palindrome Product/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Clovin on 10/03/2018.
+ */
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var largestPalindrome = function (n) {
+  return [9, 9009, 906609, 99000099, 9966006699, 999000000999, 99956644665999, 9999000000009999][n - 1] % 1337
+}
