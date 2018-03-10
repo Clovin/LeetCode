5f46ee8ec56ce72e3eb28641add2ee999c9f72cd
Clovin <wxxzhuang@gmail.com>
2018-03-10 17:25:12 +0800

    solve 438

--- 438 Find All Anagrams in a String/index.js
+++ 438 Find All Anagrams in a String/index.js
@@ -7,5 +7,39 @@
  * @return {number[]}
  */
 var findAnagrams = function (s, p) {
-  // TODO: solve
+  let result = []
+
+  search(0, s, p, p, result)
+
+  return result
+}
+
+function search (index, s, p, nowP, result) {
+  if (index === s.length) {
+    return
+  }
+
+  if (nowP.includes(s[index])) {
+    let newP = nowP
+    let i = newP.indexOf(s[index])
+    newP = newP.slice(0, i) + newP.slice(i + 1)
+
+    if (newP.length === 0) {
+      result.push(index - p.length + 1)
+
+      newP = s[index - p.length + 1]
+
+      search(index + 1, s, p, newP, result)
+    } else {
+      search(index + 1, s, p, newP, result)
+    }
+  } else if (p.includes(s[index])) {
+    let newP = nowP
+
+    newP += s[index - nowP.length - 1]
+
+    search(index, s, p, newP, result)
+  } else {
+    search(index + 1, s, p, p, result)
+  }
 }
