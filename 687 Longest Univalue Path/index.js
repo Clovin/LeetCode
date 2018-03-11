dd04dd8036a55fb6a2832be4de5362b8052441f7
Clovin <wxxzhuang@gmail.com>
2018-03-11 14:29:14 +0800

    solve 687

--- 687 Longest Univalue Path/index.js
+++ 687 Longest Univalue Path/index.js
@@ -13,6 +13,34 @@
  * @return {number}
  */
 var longestUnivaluePath = function (root) {
-  // TODO: solve
+  if (root === null) {
+    return 0
+  }
+
+  let result = 0
+  let queue = []
+  queue.push(root)
+  while (queue.length !== 0) {
+    let tempNode = queue.shift()
+    if (tempNode === null) {
+      continue
+    }
+
+    let temp = search(tempNode.left, tempNode.val) + search(tempNode.right, tempNode.val) + 1
+    if (temp > result) {
+      result = temp
+    }
+
+    queue.push(tempNode.left)
+    queue.push(tempNode.right)
+  }
+  return result - 1
 }
 
+function search (node, now) {
+  if (node === null || node.val !== now) {
+    return 0
+  }
+
+  return Math.max(search(node.left, now), search(node.right, now)) + 1
+}
