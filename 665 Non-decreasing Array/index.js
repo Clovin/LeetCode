9344cec8b2dd2d318225f8a4a6a8623a5258957a
Clovin <wxxzhuang@gmail.com>
2018-03-12 02:40:55 +0800

    solve 665

--- 665 Non-decreasing Array/index.js
+++ 665 Non-decreasing Array/index.js
@@ -10,15 +10,32 @@ var checkPossibility = function (nums) {
 
   for (let i = 0; i < nums.length - 1; i++) {
     if (nums[i] > nums[i + 1]) {
-      if (flag) {
-
-        //TODO: solve
-        flag = false
+      let [newNum1, newNum2] = [Array.from(nums), Array.from(nums)]
+      let [flag1, flag2] = [true, true]
+      if (i - 1 >= 0) {
+        newNum1.splice(i, 1, newNum1[i - 1] + 1)
+        flag1 = search(i, newNum1)
       } else {
-        return false
+        flag1 = search(1, newNum1)
       }
+
+      if (i + 2 < nums.length) {
+        newNum2.splice(i + 1, 1, newNum2[i + 2] - 1)
+        flag2 = search(i, newNum2)
+      }
+
+      return flag1 || flag2
     }
   }
 
+  return true
+}
+
+function search (index, nums) {
+  for (let i = index; i < nums.length - 1; i++) {
+    if (nums[i] > nums[i + 1]) {
+      return false
+    }
+  }
   return true
 }
